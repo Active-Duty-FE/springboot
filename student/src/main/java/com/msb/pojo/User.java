da34dbbf1ea5bdc0417f180909d66e54c62c3d68
rhkim06 <ryuhyang.kim@riman.com>
2025-03-07 10:00:19 +0900

    a

--- student/src/main/java/com/msb/pojo/User.java
+++ student/src/main/java/com/msb/pojo/User.java
@@ -4,11 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
+
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class User {
+
+public class User implements Serializable {
     private Integer id;
     private String name;
     private Clazz clazz;
